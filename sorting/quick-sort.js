957024c5ce764a005a65e39a3fda1ac2c3b369f5
Narsingh <narsinghmali123@gmail.com>
2026-03-30 10:58:54 +0530

    structure change

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -1,21 +1,16 @@
 const quickSort = (arr) => {
-                    if (arr.length<=1) return arr;
+  if (arr.length <= 1) return arr;
 
-
-
-
-
-const pivotSort = (arr, pivot) => {
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+  const pivotSort = (arr, pivot) => {
+    const left = [];
+    const right = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
     }
-  }
-  return [...left, pivot, ...right];
-};  
-
-
+    return [...left, pivot, ...right];
+  };
+};
